b0f9616a7b46cd6836fbb4c90f97e86854c2da82
Shashank Srikant <shash@mit.edu>
2022-10-25 11:37:01 -0400

    Update connect_sshfs.sh

--- connect_sshfs.sh
+++ connect_sshfs.sh
@@ -7,5 +7,5 @@ kill $(pgrep -lf sshfs);
 sudo umount -f /local/drive/folder1/;
 sudo umount -f /local/drive/folder2/;
 
-sshfs -o IdentityFile=~/.ssh/private-key.pem -o nonempty ubuntu@128.52.140.65:/home/ubuntu/ /local/drive/folder1/
+sshfs -o IdentityFile=~/.ssh/private-key.pem -o nonempty ubuntu@instance.IP.address:/home/ubuntu/ /local/drive/folder1/
 sshfs -o nonempty -o ssh_command='ssh -J username@jump.host.server.IP' username@assigned.instance.IP.address:/home/ /local/drive/folder2/
